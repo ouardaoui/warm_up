e207245a971a881e459c9fcf25fbe1c9e2921f24
ouardaoui <ayoubouardaoui3@gmail.com>
2024-08-03 18:21:05 +0100

    02 00

--- py/oop.py
+++ py/oop.py
@@ -1,5 +1,6 @@
 class Item : 
     rate = 0.8
+    all = []
     def __init__(self, name : str, price : float, count : int ) -> None:
         assert price >= 0 , f"price should be more or equal zero"
         assert count >= 0 , f"quantity should be more or equal zero"
@@ -7,14 +8,20 @@ class Item :
         self.name = name 
         self.price = price 
         self.count = count
+        Item.all.append(self)
     def calcul(self) -> float : 
         return (self.price * self.count)
     def descount(self) -> None : 
         #self.price = self.price * Item.rate 
-        self.price = self.price * self.rate 
+        self.price = self.price * self.rate
+    def __repr__(self) -> str:
+        return f"Item('{self.name}' ,{self.price} , {self.count})"
 
 item1  = Item("laptop", 100 , 3)
 item2  = Item("phone",  30 , 6)
+item3  = Item("cable",  10 , 20)
+item4  = Item("mouse",  20 , 3)
+item5  = Item("keyboard",  60 , 2)
 
 print(item1.calcul())
 # print(Item.__dict__) all attribute in class lvl 
@@ -22,5 +29,6 @@ print(item1.calcul())
 item2.descount()
 print(item2.price)
 
+print(Item.all)
 
 
