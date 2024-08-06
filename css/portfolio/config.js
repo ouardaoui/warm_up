8d0fc07af09c997f401bf2641012737fbb402e8c
ouardaoui <ayoubouardaoui3@gmail.com>
2024-08-06 11:59:43 +0100

    Stop tracking config.js and add to .gitignore

--- css/portfolio/config.js
+++
@@ -1,9 +0,0 @@
-
-const EMAILJS_CONFIG = {
-    YOUR_SERVICE_ID : "service_tkbmr3r",
-    YOUR_TEMPLATE_ID : "template_4un1ki7",
-    public_key: "9gfUjvOt8g8Jzh-JH"
-};
-
-window.EMAILJS_CONFIG = EMAILJS_CONFIG;
-//export default EMAILJS_CONFIG;
